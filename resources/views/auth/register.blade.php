93d21c98cdb90a6c605f6bb388ad480462097405
hiroaki1976 <yokota2h@gmail.com>
2024-01-23 01:04:29 +0900

    20240122_終了時点

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,41 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
+        <!-- Office Name -->
+        <div>
+            <x-input-label for="office_name" :value="__('OfficeName')" />
+            <x-text-input id="office_name" class="block mt-1 w-full" type="text" name="office_name" :value="old('office_name')" required autofocus autocomplete="office_name" />
+            <x-input-error :messages="$errors->get('office_name')" class="mt-2" />
+        </div>
+
+        <!-- Post Code -->
+        <div>
+            <x-input-label for="postcode" :value="__('Postcode')" />
+            <x-text-input id="postcode" class="block mt-1 w-full" type="text" name="postcode" :value="old('postcode')" required autofocus autocomplete="postcode" />
+            <x-input-error :messages="$errors->get('postcode')" class="mt-2" />
+        </div>
+
+        <!-- Prefecture -->
+        <div>
+            <x-input-label for="prefecture" :value="__('Prefecture')" />
+            <x-text-input id="prefecture" class="block mt-1 w-full" type="text" name="prefecture" :value="old('prefecture')" required autofocus autocomplete="prefecture" />
+            <x-input-error :messages="$errors->get('prefecture')" class="mt-2" />
+        </div>
+
+        <!-- City -->
+        <div>
+            <x-input-label for="city" :value="__('City')" />
+            <x-text-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')" required autofocus autocomplete="city" />
+            <x-input-error :messages="$errors->get('city')" class="mt-2" />
+        </div>
+
+        <!-- Town -->
+        <div>
+            <x-input-label for="town" :value="__('Town')" />
+            <x-text-input id="town" class="block mt-1 w-full" type="text" name="town" :value="old('town')" required autofocus autocomplete="town" />
+            <x-input-error :messages="$errors->get('town')" class="mt-2" />
+        </div>
+
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
