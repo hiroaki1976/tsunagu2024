6d834a6c8ef7cfa1667189ddc47223dd89a074a1
hiroaki1976 <yokota2h@gmail.com>
2024-02-20 23:37:00 +0900

    20240220_終了時点

---
+++ database/migrations/2024_02_16_163515_create_client_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('company');
+            $table->boolean('group');
+            $table->boolean('individual');
+            $table->string('name');
+            $table->string('postcode')->nullable();
+            $table->string('prefecture')->nullable();
+            $table->string('city')->nullable();
+            $table->string('town')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client');
+    }
+};
